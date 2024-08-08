56ab100dc7bd69ecb99522fc8cf95b0ccb52c237
arthur <wr80340@smail.nchu.edu.tw>
2024-08-08 17:09:39 +0800

    # modify format

--- single_machine_v2/config.py
+++ single_machine_v2/config.py
@@ -3,6 +3,7 @@ import itertools
 import numpy as np
 
 kwargs = {
+    # -------------------------------
     "action_mapping": {
         idx: combs
         for idx, combs in enumerate(
@@ -11,10 +12,13 @@ kwargs = {
             )
         )
     },
+    # -------------------------------
     "init_speed": 35,
     "min_speed": 0,
     "max_speed": 180,
+    # -------------------------------
     "num_of_eqps": 1,
+    # -------------------------------
     "learning_rate": 0.1,
     "explore_rate": 0.5,
     "learning_rate_min": 0.03,
