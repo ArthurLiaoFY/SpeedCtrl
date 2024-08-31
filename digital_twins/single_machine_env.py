ed60ed552706842e070bb3aba314b549eb996bcb
arthur <wr80340@smail.nchu.edu.tw>
2024-08-31 10:05:55 +0800

    #init unity config and modify single machine env

--- digital_twins/single_machine_env.py
+++ digital_twins/single_machine_env.py
@@ -6,7 +6,9 @@ class SNGenerator(sim.Component):
         while True:
             while head_buffer.available_quantity() <= 0:
                 self.standby()
-            self.hold(4)
+            self.request(conveyor1)
+            self.hold(2)
+            self.release()
             SN().enter(head_buffer)
 
 
@@ -22,9 +24,11 @@ class SNSink(sim.Component):
             while tail_buffer.available_quantity() == tail_buffer.capacity.value:
                 self.standby()
             product = self.from_store(tail_buffer)
-            product.activate()
+            self.request(conveyor2)
             self.hold(4)
+            self.release()
             product.passivate()
+            env.total_prod_amount += 1
 
 
 class Machine(sim.Component):
@@ -58,7 +62,7 @@ class Machine(sim.Component):
                 self.activate()
             self.normal_production()
 
-            self.hold(sim.Uniform(2, 5))
+            self.hold(sim.Exponential(5))
 
             while tail_buffer.available_quantity() <= 0:
                 self.waiting_for_seats()
@@ -71,18 +75,24 @@ class Machine(sim.Component):
             self.to_store(tail_buffer, product)
 
 
-env = sim.Environment(trace=False)
-SNGenerator()
-Machine()
-SNSink()
+env = sim.Environment(trace=True)
+env.total_prod_amount = 0
+
+sn_generator = SNGenerator(name="產品發射器")
+conveyor1 = sim.Resource("前方傳輸帶")
 head_buffer = sim.Store(name="前方緩存區", capacity=8)
+machine = Machine(name="雷射焊錫機")
 tail_buffer = sim.Store(name="後方緩存區", capacity=8)
+conveyor2 = sim.Resource("後方傳輸帶")
+sn_sink = SNSink(name="產品接收器")
+
+
 status_0 = sim.State(name="正常生產", value=False)
 status_4 = sim.State(name="待料", value=False)
 status_5 = sim.State(name="滿料", value=False)
 
 
-env.run(till=5000)
+env.run(till=100)
 status_0.print_statistics()
 status_4.print_statistics()
 status_5.print_statistics()
@@ -90,3 +100,5 @@ status_5.print_statistics()
 
 head_buffer.print_statistics()
 tail_buffer.print_statistics()
+
+print(env.total_prod_amount)
