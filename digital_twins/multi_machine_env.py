f03e81bb7717db365c7a108c160affa234c76174
arthur <wr80340@smail.nchu.edu.tw>
2024-09-03 15:03:53 +0800

    #multi done

--- digital_twins/multi_machine_env.py
+++ digital_twins/multi_machine_env.py
@@ -6,12 +6,11 @@ import salabim as sim
 class SNGenerator(sim.Component):
     def process(self):
         while True:
-            while head_buffer.available_quantity() <= 0:
+            while simulate_config["machine_1"]["head_buffer"].available_quantity() <= 0:
                 self.standby()
-            self.request((conveyor1, 1))
-            self.hold(2)
-            self.release((conveyor1, 1))
-            SN().enter(head_buffer)
+            product = SN()
+            self.hold(1 / conveyer_speed)
+            self.to_store(simulate_config["machine_1"]["head_buffer"], product)
 
 
 class SN(sim.Component):
@@ -19,67 +18,134 @@ class SN(sim.Component):
         while True:
             self.passivate()
 
+    def animation_objects(self, id):
+        """
+        for animation
+        """
+        if id == "text":
+            animate_text = sim.AnimateText(
+                text=self.name(), textcolor="fg", text_anchor="nw"
+            )
+            return 15, 0, animate_text
+        else:
+            animate_rectangle = sim.AnimateRectangle(
+                (
+                    -20,
+                    0,
+                    20,
+                    20,
+                ),  # (x lower left, y lower left, x upper right, y upper right)
+                text=self.name(),
+                fillcolor=id,
+                textcolor="white",
+                arg=self,
+            )
+            return 0, 30, animate_rectangle
 
-class SNSink(sim.Component):
+
+class Conveyer(sim.Component):
+    """
+    send product from machine A tail buffer area to machine B head buffer area
+    """
+
+    def __init__(
+        self, from_idx: int, to_idx: int, conveyer_speed: int, *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.from_idx = from_idx
+        self.to_idx = to_idx
+        self.conveyer_speed = conveyer_speed
+
     def process(self):
-        while True:
-            while tail_buffer.available_quantity() == tail_buffer.capacity.value:
+        if self.from_idx != 0:
+            while (
+                len(
+                    simulate_config.get(f"machine_{self.from_idx}", {}).get(
+                        "tail_buffer"
+                    )
+                )
+                <= 0
+            ):
                 self.standby()
-            product = self.from_store(tail_buffer)
-            self.request((conveyor2, 1))
-            self.hold(4)
-            self.release((conveyor2, 1))
-            product.passivate()
-            env.total_prod_amount += 1
+
+            product = self.from_store(
+                simulate_config[f"machine_{self.from_idx}"]["tail_buffer"]
+            )
+
+        product.hold(1 / self.conveyer_speed)
+
+        if self.from_idx == num_of_machine:
+            # to sink
+            self.to_store(simulate_config["sn_receiver"], product)
+        else:
+            while (
+                simulate_config.get(f"machine_{self.to_idx}", {})
+                .get("head_buffer")
+                .available_quantity()
+                == 0
+            ):
+                self.standby()
+            self.to_store(
+                simulate_config[f"machine_{self.to_idx}"]["head_buffer"], product
+            )
 
 
 class Machine(sim.Component):
-    def __init__(self, machine_order: int = 0, speed: int = 50000, *args, **kwargs):
+    def __init__(self, machine_idx: int, machine_speed: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.machine_order = machine_order
-        self.speed = speed
 
-    @staticmethod
-    def waiting_for_materials():
-        status_0.set(value=False)
-        status_4.set(value=True)
-        status_5.set(value=False)
+        self.machine_idx = machine_idx
+        self.machine_speed = machine_speed
 
-    @staticmethod
-    def waiting_for_seats():
-        status_0.set(value=False)
-        status_4.set(value=False)
-        status_5.set(value=True)
+    def switch_to_status(self, status: int):
+        for status_code in simulate_config[f"machine_{self.machine_idx}"][
+            "status"
+        ].keys():
+            if status_code == status:
+                simulate_config[f"machine_{self.machine_idx}"]["status"][
+                    status_code
+                ].set(value=True)
 
-    @staticmethod
-    def normal_production():
-        status_0.set(value=True)
-        status_4.set(value=False)
-        status_5.set(value=False)
+            else:
+                simulate_config[f"machine_{self.machine_idx}"]["status"][
+                    status_code
+                ].set(value=False)
 
     def process(self):
         while True:
-            while len(head_buffer) == 0:
-                self.waiting_for_materials()
+            while (
+                len(
+                    simulate_config.get(f"machine_{self.machine_idx}", {}).get(
+                        "head_buffer"
+                    )
+                )
+                == 0
+            ):
+                self.switch_to_status(status=4)
                 self.standby()
 
-            product = self.from_store(head_buffer)
+            product = self.from_store(
+                simulate_config[f"machine_{self.machine_idx}"]["head_buffer"]
+            )
 
-            if self.ispassive():
-                self.activate()
-            self.normal_production()
+            self.switch_to_status(status=0)
 
-            self.hold(sim.Exponential(5))
+            self.hold(sim.Exponential(1 / self.machine_speed))
 
-            while tail_buffer.available_quantity() <= 0:
-                self.waiting_for_seats()
+            while (
+                simulate_config.get(f"machine_{self.machine_idx}", {})
+                .get("tail_buffer")
+                .available_quantity()
+                <= 0
+            ):
+                self.switch_to_status(status=5)
                 self.standby()
 
-            if self.ispassive():
-                self.activate()
-            self.normal_production()
+            self.switch_to_status(status=0)
 
-            self.to_store(tail_buffer, product)
+            self.to_store(
+                simulate_config[f"machine_{self.machine_idx}"]["tail_buffer"], product
+            )
 
 
 # data_file_path = "./digital_twins"
@@ -88,26 +154,59 @@ class Machine(sim.Component):
 
 env = sim.Environment(trace=True)
 env.total_prod_amount = 0
-num_of_machine = 5
-
+num_of_machine = 3
+conveyer_speed = 1 / 2
+machine_speed = 1 / 5
 
 simulate_config = {
     **{"sn_feeder": SNGenerator(name="半成品發射器")},
     **{
         f"machine_{i+1}": {
-            "machine": Machine(name=f"設備({i+1})"),
-            "head_buffer": sim.Resource(f"設備({i+1}) 前方緩存區", capacity=8),
-            "tail_buffer": sim.Resource(f"設備({i+1}) 後方緩存區", capacity=8),
-            "status_0": sim.State(name=f"設備({i+1}) 正常生產", value=False),
-            "status_4": sim.State(name=f"設備({i+1}) 待料", value=False),
-            "status_5": sim.State(name=f"設備({i+1}) 滿料", value=False),
+            "machine": Machine(
+                name=f"設備({i+1})", machine_idx=i + 1, machine_speed=machine_speed
+            ),
+            "tail_buffer": sim.Store(f"設備({i+1}) 後方緩存區", capacity=8),
+            "head_buffer": sim.Store(f"設備({i+1}) 前方緩存區", capacity=8),
+            "status": {
+                status_code: sim.State(name=cn_name, value=False)
+                for status_code, cn_name in zip(
+                    range(-1, 13),
+                    [
+                        "未連線",
+                        "正常",
+                        "故障",
+                        "暫停",
+                        "待機",
+                        "待料",
+                        "滿料",
+                        "材料低位",
+                        "換線",
+                        "缺料",
+                        "待啟動",
+                        "安全停機",
+                        "品質停機",
+                        "調機",
+                    ],
+                )
+            },
         }
         for i in range(num_of_machine)
     },
-    **{"sn_receiver": SNSink(name="成品接收器")},
+    **{
+        f"conveyer_{i}_{i+1}": {
+            "conveyer": Conveyer(
+                name=f"傳輸帶({i}至{i+1})",
+                from_idx=i,
+                to_idx=i + 1,
+                conveyer_speed=conveyer_speed,
+            ),
+        }
+        for i in range(1, num_of_machine + 1)
+    },
+    **{"sn_receiver": sim.Store(name="成品接收器")},
 }
 connection_config = {}
 
 
-# env.run(till=100)
+env.run(till=100)
 # print(env.total_prod_amount)
