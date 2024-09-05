8dc49d2c77a9cfa63296ccc3118543b0719549ef
arthur <wr80340@smail.nchu.edu.tw>
2024-09-05 09:18:56 +0800

    #init new config

--- line_solution/config.py
+++ line_solution/config.py
@@ -2,8 +2,8 @@
 import json
 from collections import defaultdict
 
-# unity_config = json.loads(open("./unity_config.json").read())
-unity_config = json.loads(open("./line_solution/unity_config.json").read())
+unity_config = json.loads(open("./unity_config.json").read())
+# unity_config = json.loads(open("./line_solution/unity_config.json").read())
 num_of_machine = len(unity_config.get("Layoutdata").get("Device"))
 
 id_to_machine_map = {
@@ -55,7 +55,7 @@ simulate_conveyer_config = {
     **{
         f"{target}#{source}": {
             "conveyer_name": f"傳輸帶({simulate_machine_config.get(target, {}).get('machine_name')} --> {simulate_machine_config.get(source, {}).get('machine_name')})",
-            "conveyer_cycletime": 1,
+            "conveyer_cycletime": 0.5,
             "conveyer_from": target,
             "conveyer_to": source,
         }
